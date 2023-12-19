b7c3ffba9ddfc98eb31e44ce420b687434b79e7c
Plump Albert <plumpalbert@gmail.com>
2023-12-19 15:04:29 +0300

    updated `MovieDownload` struct

--- lib/radarr.go
+++ lib/radarr.go
@@ -1,20 +1,45 @@
 package lib
 
+import "time"
+
 type MovieDownloaded struct {
-	Movie Movie     `json:"movie"`
-	File  MovieFile `json:"movieFile"`
+	Movie              Movie       `json:"movie"`
+	File               MovieFile   `json:"movieFile"`
+	RemoteMovie        RemoteMovie `json:"remoteMovie"`
+	IsUpgrade          bool        `json:"isUpgrade"`
+	DownloadClient     string      `json:"downloadClient"`
+	DownloadClientType string      `json:"downloadClientType"`
+	DownloadId         string      `json:"downloadId"`
+	EventType          string      `json:"eventType"`
 }
 
 type Movie struct {
-	Id    int    `json:"id"`
+	Id          int       `json:"id"`
+	Title       string    `json:"title"`
+	Year        int       `json:"year"`
+	ReleaseDate time.Time `json:"releaseDate"`
+	FolderPath  string    `json:"folderPath"`
+	TMDB        int       `json:"tmdbId"`
+	IMDB        string    `json:"imdbId"`
+}
+
+type RemoteMovie struct {
+	TMDB  int    `json:"tmdbId"`
+	IMDB  string `json:"imdbId"`
 	Title string `json:"title"`
 	Year  int    `json:"year"`
-	IMDB  string `json:"imdbId"`
 }
 
 type MovieFile struct {
-	Id      int    `json:"id"`
-	Quality string `json:"quality"`
+	Id             int    `json:"id"`
+	RelativePath   string `json:"relativePath"`
+	Path           string `json:"path"`
+	Quality        string `json:"quality"`
+	QualityVersion int    `json:"qualityVersion"`
+	ReleaseGroup   string `json:"releaseGroup"`
+	SceneName      string `json:"sceneName"`
+	IndexerFlags   string `json:"indexerFlags"`
+	Size           int    `json:"size"`
 }
 
 // {
